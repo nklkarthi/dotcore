a455eb7101cf4e6c18302aa37258fb4ff9b140cc
nklkarthi <nklkarthi@gmail.com>
2023-02-16 19:36:33 +0800

    Initial commit - main branch

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string response;
+Console.WriteLine("What's your name");
+response = Console.ReadLine();
+Console.WriteLine($"Enjoy the course, {response}");
+
+OperatingSystem thisOs = Environment.OSVersion;
+Console.WriteLine(thisOs.VersionString);
+
+
